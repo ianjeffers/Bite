e2dcd133a63271c9f7aefaee77ebce342acb5c83
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2023-06-20 21:55:58 -0700

    Nothing to see here carry on

--- BiteApp/App.js
+++ BiteApp/App.js
@@ -1,25 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import { getGpt3Data, getWikipediaData } from './api';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
-    const [educationContext, setEducationContext] = useState(null);
-
-    useEffect(() => {
-        if (educationContext) {
-            const fetchData = async () => {
-                const wikipediaData = await getWikipediaData(educationContext);
-                const gpt3Data = await getGpt3Data(wikipediaData[2][0]);
-                console.log(gpt3Data);
-            }
-            fetchData();
-        }
-    }, [educationContext]);
-
     return (
-        <View>
-            <Text>Bite - Micro-learning App</Text>
-            {/* Additional components and layout here */}
-        </View>
+        <HomeScreen></HomeScreen>
     );
 }
