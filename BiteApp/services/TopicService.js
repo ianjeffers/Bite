ca9f0d1f05986fd2c201ff4cd158e978922d6aa2
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2023-06-25 18:18:42 -0700

    Let's integrate the backend!

--- BiteApp/services/TopicService.js
+++ BiteApp/services/TopicService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class TopicService {
   async generateRelatedTopics(topic, type) {
     try {
-        const response = await axios.post(`http://localhost:5000/topics`, { topic, reason: type });
+        const response = await axios.post(`18.217.80.196:5000/topics`, { topic, reason: type });
         return response.data.related_topics;
     } catch (error) {
       console.error('Failed to fetch related topics:', error);
