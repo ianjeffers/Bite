d5ec77493e8c47c3531cb20a4887351e088a3f49
ianjeffers <71468572+ianjeffers@users.noreply.github.com>
2023-06-25 18:38:34 -0700

    This oughta work

--- BiteApp/services/ApiService.js
+++ BiteApp/services/ApiService.js
@@ -1,27 +1,33 @@
 import axios from 'axios';
 
-const ApiService = axios.create({
-  baseURL: 'http://18.217.80.196:5000/', 
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*', 
-  },
-  timeout: 10000, 
-});
-
-ApiService.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
-
-ApiService.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  if (error.response && error.response.status === 500) {
-    error.message = 'Server Error, Please try again later.';
+class ApiService {
+  
+  async get(endpoint, params) {
+    try {
+      const response = await axios.get(`http://18.217.80.196:5000/${endpoint}`, { params });
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 500) {
+        error.message = 'Server Error, Please try again later.';
+      }
+      console.error('Failed to perform GET request:', error);
+      throw error;
+    }
   }
-  return Promise.reject(error);
-});
+  
+  async post(endpoint, data) {
+    try {
+      const response = await axios.post(`http://18.217.80.196:5000/${endpoint}`, data);
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 500) {
+        error.message = 'Server Error, Please try again later.';
+      }
+      console.error('Failed to perform POST request:', error);
+      throw error;
+    }
+  }
+  
+}
 
-export default ApiService;
+export default new ApiService();
